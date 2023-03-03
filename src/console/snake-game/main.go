7f34c317b73f03b16c81b04fc3d5806cc77ca949
clucle <wjdenwls123@gmail.com>
2023-03-04 00:35:55 +0900

    feat: clear screen each update
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/console/snake-game/main.go
+++ src/console/snake-game/main.go
@@ -9,6 +9,7 @@ func main() {
 	ticker := time.NewTicker(time.Millisecond * 100)
 	go func() {
 		for t := range ticker.C {
+			fmt.Print("\033[H\033[2J") // clear screen
 			fmt.Println("current time", t)
 		}
 	}()
