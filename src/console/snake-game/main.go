1ed281103102e9274af056e8bc2ce370d95e82b8
clucle <wjdenwls123@gmail.com>
2023-03-05 17:24:07 +0900

    feat: print elapsed time & clear screen afger game
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/console/snake-game/main.go
+++ src/console/snake-game/main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
 
+func clearScreen() {
+	osString := runtime.GOOS
+	if osString == "linux" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else if osString == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
 func main() {
 	ticker := time.NewTicker(time.Millisecond * 100)
+	done := make(chan bool)
+
 	go func() {
-		for t := range ticker.C {
-			fmt.Print("\033[H\033[2J") // clear screen
-			fmt.Println("current time", t)
+		elpasedTime := 0
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				elpasedTime += 1
+				fmt.Print("\033[H\033[2J")
+				fmt.Println("Elapsed Time : ", elpasedTime)
+			}
 		}
 	}()
 
 	input := 0
 	fmt.Scanln(&input)
 	ticker.Stop()
+	done <- true
+
+	clearScreen()
 }
