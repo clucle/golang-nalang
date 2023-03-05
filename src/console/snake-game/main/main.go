86c6a34545a2e69f107ea666ba51b77f71d3678b
clucle <wjdenwls123@gmail.com>
2023-03-05 22:25:14 +0900

    style: divide game & main package
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- src/console/snake-game/main/main.go
+++ src/console/snake-game/main/main.go
@@ -1,72 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/clucle/golang-nalang/src/console/snake-game/snakegame"
-	"github.com/eiannone/keyboard"
 )
 
 func main() {
-	snakegame.ClearScreen()
-
-	ticker := time.NewTicker(time.Millisecond * 200)
-	done := make(chan bool)
-
-	// set key events
-	keysEvents, err := keyboard.GetKeys(10)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
-
-	type KeyState int
-	const (
-		KeyUp KeyState = 1 + iota
-		KeyDown
-		KeyLeft
-		KeyRight
-	)
-	var keyState = KeyUp
-
-	go func() {
-		elpasedTime := 0
-		for {
-			select {
-			case <-done:
-				return
-
-			case <-ticker.C:
-				elpasedTime += 1
-				snakegame.ClearScreen()
-				// snakegame.Display()
-				fmt.Printf("%d", keyState)
-			}
-		}
-	}()
-
-	for {
-		event := <-keysEvents
-		if event.Err != nil {
-			panic(event.Err)
-		}
-		if event.Key == keyboard.KeyEsc {
-			ticker.Stop()
-			done <- true
-			break
-		} else if event.Key == keyboard.KeyArrowDown || event.Key == 65516 {
-			keyState = KeyDown
-		} else if event.Key == keyboard.KeyArrowUp || event.Key == 65517 {
-			keyState = KeyUp
-		} else if event.Key == keyboard.KeyArrowLeft || event.Key == 65515 {
-			keyState = KeyLeft
-		} else if event.Key == keyboard.KeyArrowRight || event.Key == 65514 {
-			keyState = KeyRight
-		}
-	}
-
+	game := &snakegame.Game{}
+	game.Init()
+	game.Run()
 	snakegame.ClearScreen()
 }
--- src/console/snake-game/snakegame/game.go
+++ src/console/snake-game/snakegame/game.go
@@ -1,20 +1,99 @@
 package snakegame
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/eiannone/keyboard"
+)
 
 const boardSize = 13
 
+type KeyState int
+
+const (
+	KeyUp KeyState = 1 + iota
+	KeyDown
+	KeyLeft
+	KeyRight
+)
+
 type Game struct {
-	board [boardSize][boardSize]int
+	ticker      *time.Ticker
+	done        chan bool
+	board       [boardSize][boardSize]int
+	keyState    KeyState
+	elapsedTime int
 }
 
-func Display(game Game) {
-	var ret string
+func (game *Game) Init() {
+	game.ticker = time.NewTicker(time.Millisecond * 160)
+	game.done = make(chan bool)
+	game.elapsedTime = 0
+
 	for row := 0; row < boardSize; row++ {
 		for col := 0; col < boardSize; col++ {
-			ret += "▢ "
+			game.board[row][col] = 0
 		}
-		ret += "\n"
 	}
-	fmt.Println(ret)
+}
+
+func (game *Game) Run() {
+	// set key events
+	keysEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-game.done:
+				return
+
+			case <-game.ticker.C:
+				game.elapsedTime++
+				ClearScreen()
+				game.Display()
+				fmt.Printf("%d", game.keyState)
+			}
+		}
+	}()
+
+	game.keyState = KeyUp
+
+	for {
+		event := <-keysEvents
+		if event.Err != nil {
+			panic(event.Err)
+		}
+		if event.Key == keyboard.KeyEsc {
+			game.ticker.Stop()
+			game.done <- true
+			break
+		} else if event.Key == keyboard.KeyArrowDown || event.Key == 65516 {
+			game.keyState = KeyDown
+		} else if event.Key == keyboard.KeyArrowUp || event.Key == 65517 {
+			game.keyState = KeyUp
+		} else if event.Key == keyboard.KeyArrowLeft || event.Key == 65515 {
+			game.keyState = KeyLeft
+		} else if event.Key == keyboard.KeyArrowRight || event.Key == 65514 {
+			game.keyState = KeyRight
+		}
+	}
+
+}
+
+func (game *Game) Display() {
+	display := fmt.Sprintf("Elapsed Time : %d\n", game.elapsedTime)
+	for row := 0; row < boardSize; row++ {
+		for col := 0; col < boardSize; col++ {
+			display += "▢ "
+		}
+		display += "\n"
+	}
+	fmt.Println(display)
 }
